a21f7598088b35b9ec5fea35de96b327ef3c083c
chris-rios <rioschm@gmail.com>
2020-09-07 18:47:37 -0400

    Speed up time, add in custom result from time calculator

--- src/timeCalculator.js
+++ src/timeCalculator.js
@@ -2,14 +2,37 @@
  * Used to convert real time to game time
  */
 const startTime = Date.now();
-const TIME_MULTIPLIER = 10;
+const TIME_MULTIPLIER = 60;
 
+
+const calculateDayString = (dayValue) => {
+    switch(dayValue) {
+        case 0: 
+            return 'Sunday';
+        case 1: 
+            return 'Monday';
+        case 2:
+            return 'Tuesday';
+        case 3:
+            return 'Wednesday';
+        case 4:
+            return 'Thursday';
+        case 5:
+            return 'Friday';
+        case 6:
+            return 'Saturday';
+    }
+}
 
  const calculateVillageTime = () => {
     const currentTime = Date.now();
     const timeDifference = (Date.now() - startTime) * TIME_MULTIPLIER;
     const newTime = new Date(timeDifference + startTime);
-
+    return {
+        day: calculateDayString(newTime.getDay()),
+        hours: newTime.getHours(),
+        minutes: newTime.getMinutes()
+    }
     return newTime;
  }
 
