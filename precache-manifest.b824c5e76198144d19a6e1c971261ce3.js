605f5bff1982ca272279dfb4965c54469a8d3b4b
chris-rios <rioschm@gmail.com>
2020-09-13 22:58:26 -0400

    Updates

---
+++ precache-manifest.b824c5e76198144d19a6e1c971261ce3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5f9941a8058b66efd5133d52c726189",
+    "url": "/looper/index.html"
+  },
+  {
+    "revision": "9a383adb63fe941c48ae",
+    "url": "/looper/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dee90b842426aa47ef3f",
+    "url": "/looper/static/js/2.7c5f6a7d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/looper/static/js/2.7c5f6a7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a383adb63fe941c48ae",
+    "url": "/looper/static/js/main.38701961.chunk.js"
+  },
+  {
+    "revision": "8f0fbd5e440235bb75bb",
+    "url": "/looper/static/js/runtime-main.a90440bc.js"
+  }
+]);
